3e16d896746938c0761e60d577f938e23aab75ad
U2tzJTNE <u2tzjtne@gmail.com>
2021-05-18 13:58:30 +0800

    first commit

---
+++ app/src/main/java/com/u2tzjtne/pdfreader/ReaderActivity.kt
@@ -0,0 +1,12 @@
+package com.u2tzjtne.pdfreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reader)
+    }
+}
